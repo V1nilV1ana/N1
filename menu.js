654b108e8b5f74d6b4cc8366c6c2d81589d64888
Vinicius <vinicius.lviana@hotmail.com>
2022-09-25 19:25:05 -0300

    button mobile update

---
+++ menu.js
@@ -0,0 +1,14 @@
+  const nav_link = document.querySelector(".nav_link");
+  const nav_toggle = document.querySelector(".mobile_nav");
+
+  nav_toggle.addEventListener('click', () => {
+    const visibility = nav_link.getAttribute("data-visible");
+
+    if(visibility === "false"){
+        nav_link.setAttribute("data-visible", true);
+        nav_link.setAttribute("aria-expanded", true);
+    } else if(visibility === "true") {
+        nav_link.setAttribute("data-visible", false);
+        nav_link.setAttribute("aria-expanded", false);
+    }
+  });
